65d4d37d3faf3e09d37c11ddcd779231ad9da9a2
Nicolò Zandarin <nikzanda@hotmail.it>
2024-08-12 01:10:22 +0200

    basename & watermark

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,33 @@
 import React, { Suspense } from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
 import './App.css';
+import { Watermark } from 'antd';
 import { AuthenticationContext } from './contexts';
 import { LoadingPage } from './views';
+import { getYears } from './utils';
 
 const AuthenticatedLayout = React.lazy(() => import('./layouts/AuthenticatedLayout'));
 const UnauthenticatedLayout = React.lazy(() => import('./layouts/UnauthenticatedLayout'));
 
+const { REACT_APP_BASENAME } = process.env;
+const showWatermark = parseInt(REACT_APP_BASENAME!, 10) - 1 < getYears()[0];
+
 const App: React.FC = () => {
   const { currentUser, loading } = React.useContext(AuthenticationContext);
 
-  return (
-    <Suspense fallback={<LoadingPage />}>
-      <Router>
+  const routerContent = React.useMemo(() => {
+    const result = (
+      <Router basename={`/${REACT_APP_BASENAME}`}>
         {!loading && !currentUser && <UnauthenticatedLayout />}
         {currentUser && <AuthenticatedLayout />}
       </Router>
+    );
+    return result;
+  }, [currentUser, loading]);
+
+  return (
+    <Suspense fallback={<LoadingPage />}>
+      {showWatermark ? <Watermark content={REACT_APP_BASENAME}>{routerContent}</Watermark> : routerContent}
     </Suspense>
   );
 };
